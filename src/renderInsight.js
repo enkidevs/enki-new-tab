ddbbba9df6f466e88324b448e3b3870cc5e1f6c9
Mathieu Dutour <mathieu@dutour.me>
2016-05-09 16:52:53 +0100

    static twitter button

--- src/renderInsight.js
+++ src/renderInsight.js
@@ -5,7 +5,10 @@ export default function render (insight) {
   const source = document.getElementById('insight-template').innerHTML
   const template = Handlebars.compile(source)
 
-  insight = {...insight, ...parseInsightMarkdown(insight)}
+  insight = {...insight, ...parseInsightMarkdown(insight),
+    encodedHeadline: encodeURI(insight.headline),
+    encodedURL: encodeURI(`https://insights.enki.com/insight/${insight.id}`)
+  }
 
   const html = template(insight)
 

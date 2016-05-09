920efb3fd363a73f05667cafc6ae8802cd8cce55
Johnny Magrippis <j@magrippis.com>
2016-05-09 16:40:01 +0100

    Stores and uses only the latest insight, imports endpoint and token from a separate file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import renderInsight from './renderInsight'
+import { config } from './config.js'
 
 let enkiData = null
 
@@ -7,35 +8,29 @@ chrome.storage.local.get('enkiNewTab', (data) => {
     enkiData = data.enkiNewTab
   }
 
-  if (enkiData) {
-    return renderInsights(enkiData.insights)
+  if (enkiData && enkiData.insight) {
+    return renderInsight(enkiData.insight)
   }
 
   let headers = {
-    'x-access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NTQzNzM1MDYxY2EyOWVhMDAzYTBjMjMiLCJleHAiOjE0OTQyNjMyMDA4MjZ9.XF9KaZaYjxbwZvLDF7-Sd-qTAou8hTIOqUGxVoKLaiE'
+    'x-access-token': config.accessToken
   }
 
-  fetch('https://insights.enki.com/api/insights/live/0', { headers })
+  fetch(config.endpoint, { headers })
     .then((response) => {
       if (response.ok) {
         return response.json().then((insights) => {
           console.log(insights)
           enkiData = {
-            insights,
+            insight: insights.items[0],
             expiry: Date.now() + 86400000
           }
 
           chrome.storage.local.set({ enkiNewTab: enkiData })
-          renderInsights(insights)
+          renderInsight(insights.items[0])
         })
       }
 
       // TODO: error handling
     })
 })
-
-function renderInsights (insights) {
-  insights.items.map(insight => {
-    renderInsight(insight)
-  })
-}

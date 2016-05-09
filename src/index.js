f49d2d6ff2aa7336ca57d3d805ea9720d51082d6
Johnny Magrippis <j@magrippis.com>
2016-05-09 15:30:44 +0100

    Renders the insight on the new tab

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
+import renderInsight from './renderInsight'
+
 let enkiData = null
 
-enkiData = chrome.storage.local.get('enkiNewTab', (data) => {
-  console.log(data)
+chrome.storage.local.get('enkiNewTab', (data) => {
   if (data && data.enkiNewTab && data.enkiNewTab.expiry > Date.now()) {
     enkiData = data.enkiNewTab
   }
 
-  if (!enkiData) {
-    let headers = {
-      'x-access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NTQzNzM1MDYxY2EyOWVhMDAzYTBjMjMiLCJleHAiOjE0OTQyNjMyMDA4MjZ9.XF9KaZaYjxbwZvLDF7-Sd-qTAou8hTIOqUGxVoKLaiE'
-    }
-
-    console.log('Calling API')
-
-    fetch('https://insights.enki.com/api/insights/live/0', { headers })
-      .then((response) => {
-        if (response.ok) {
-          return response.json().then((insights) => {
-            console.log(insights)
-            enkiData = {
-              insights,
-              expiry: Date.now() + 86400000
-            }
-
-            chrome.storage.local.set({ enkiNewTab: enkiData })
-          })
-        }
-      })
+  if (enkiData) {
+    return renderInsights(enkiData.insights)
   }
+
+  let headers = {
+    'x-access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NTQzNzM1MDYxY2EyOWVhMDAzYTBjMjMiLCJleHAiOjE0OTQyNjMyMDA4MjZ9.XF9KaZaYjxbwZvLDF7-Sd-qTAou8hTIOqUGxVoKLaiE'
+  }
+
+  fetch('https://insights.enki.com/api/insights/live/0', { headers })
+    .then((response) => {
+      if (response.ok) {
+        return response.json().then((insights) => {
+          console.log(insights)
+          enkiData = {
+            insights,
+            expiry: Date.now() + 86400000
+          }
+
+          chrome.storage.local.set({ enkiNewTab: enkiData })
+          renderInsights(insights)
+        })
+      }
+
+      // TODO: error handling
+    })
 })
+
+function renderInsights (insights) {
+  insights.items.map(insight => {
+    renderInsight(insight)
+  })
+}
--- src/renderInsight.js
+++ src/renderInsight.js
@@ -1,6 +1,6 @@
 import Handlebars from 'handlebars'
 
-export function render (insight) {
+export default function render (insight) {
   const source = document.getElementById('insight-template').innerHTML
   const template = Handlebars.compile(source)
   const html = template(insight)
